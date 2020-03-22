9be09d7b5dec1cb9d499984d4ae05d48498133c5
MathiasOoi <60244997+MathiasOoi@users.noreply.github.com>
2020-03-22 12:19:57 -0800

    conversions

--- Unit_Conversion/conversions.py
+++ Unit_Conversion/conversions.py
@@ -1,43 +1,35 @@
 import random
+from collections import defaultdict
 
 with open("conversions.txt", "r") as fin:
     conversions = fin.readlines()
-    fin.close()
-    d = {}
-    for i in range(len(conversions)):
-        tem = {}
-        line = conversions[i].split()
-        if line[0] in d.keys():
-            d[line[0]].update({line[1]:float(line[2])})
-        else:
-            tem[line[1]] = float(line[2])
-            d[line[0]] = tem
-
+    d = defaultdict(dict)
+    # Creates dict of dicts
+    for i in conversions:
+        # Iterate over every conversion and maps unit to dict of another unit to conversion factor
+        line = i.split()
+        d[line[0]] = {line[1]: float(line[2])}
+    print(d)
 
 with open("requests.txt", "r") as requests:
     r = []
     for line in requests:
         r.append(line.split())
-    requests.close()
 
 
 def dict_reciprocal(dictionary):
+    # Adds reciprocal of a given dict of dicts
     d_copy = dictionary.copy()
     for key, d in d_copy.items():
-        temp_dict = {}
-        l = list(d.items())
-        for i in range(0, len(l)):
-            value = l[i][0]
-            multiplier = l[i][1]
-            temp_dict[key] = 1/multiplier
+        for value, multiplier in d.items():
             if value in dictionary.keys():
-                dictionary[value].update(temp_dict)
+                dictionary[value].update({key: 1/multiplier})
             else:
-                dictionary[value] = temp_dict
+                dictionary[value] = {key: 1/multiplier}
 
 
-def path_correct(path, start, end):
-    if path[0] == start and path[-1] == end:
+def path_correct(path, end):
+    if path[-1] == end:
         return True
     return False
 
@@ -45,14 +37,10 @@ def path_correct(path, start, end):
 def find_path(start, end):
     path = [start]
     multiplier = 1
-    while not path_correct(path, start, end):
-        if len(d[path[-1]]) == 1:
-            next_key = list(d[path[-1]].keys())[0]
-            multiplier *= list(d[path[-1]].values())[0]
-        else:
-            index = random.randint(0, len(d[path[-1]]) - 1)
-            next_key = list(d[path[-1]].keys())[index]
-            multiplier *= d[path[-1]][next_key]
+    while not path_correct(path, end):
+        index = random.randint(0, len(d[path[-1]]) - 1)
+        next_key = list(d[path[-1]].keys())[index]
+        multiplier *= d[path[-1]][next_key]
         path.append(next_key)
     return multiplier
 
--- test.py
+++ test.py
@@ -1,11 +1,3 @@
-dictionary = {1:{2:3, 4:5}}
-d_copy = dictionary.copy()
-for key, d in d_copy.items():
-    temp_dict = {}
-    print(list(d.items()))
-    x = list(d.items())
-    for i in range(0, len(x)):
-        print(x[i][0])
-        print(x[i][1])
-
+import random
+print(random.randint(0,0))
 
