d4b8532ff2da98a1685856f694d5f2f203b9c223
MathiasOoi <60244997+MathiasOoi@users.noreply.github.com>
2020-04-01 17:14:47 -0800

    Update conversions.py

--- Unit_Conversion/conversions.py
+++ Unit_Conversion/conversions.py
@@ -1,5 +1,5 @@
 from collections import defaultdict
-with open("conversions.txt", "r") as fin:
+with open("conversions.txt") as fin:
     d = defaultdict(dict)
     # Creates dict of dicts
     for i in fin.readlines():
@@ -7,54 +7,50 @@ with open("conversions.txt", "r") as fin:
         line = i.split()
         d[line[0]][line[1]] = float(line[2])
 
-with open("requests.txt", "r") as requests:
+with open("requests.txt") as requests:
     r = [line.split() for line in requests]
 
 
 def dict_reciprocal(dictionary):
     # Adds reciprocal of a given dict of dicts
-    d_copy = dictionary.copy()
-    for key, d in d_copy.items():
+    for key, d in dictionary.copy().items():
         for value, multiplier in d.items():
-            if value in dictionary.keys():
-                dictionary[value].update({key: 1/multiplier})
-            else:
-                dictionary[value] = {key: 1/multiplier}
+            dictionary[value][key] = 1/multiplier
 
 
 def find_path(start, end, dictionary):
     # Uses BFS to find a path
-    q = [(start, [start])]
-    # Tuple of last vertex visited and path
-    while len(q) != 0:
-        (v, path) = q.pop(0)
-        for next in dictionary[v].keys() - set(path):
-            if next == end:
-                # Once correct path is discovered return it
-                return path + [next]
-            else:
-                # If the path isn't discovered append visited vertex and path plus vertex
-                q.append((next, path + [next]))
+    # Initialize queue with start inside
+    q = [[start]]
+    while q:
+        # Get and remove oldest path in queue
+        path = q.pop(0)
+        # If the path is correct return it
+        if path[-1] == end:
+            return path
+
+        for next in list(dictionary[path[-1]].keys()):
+            if next not in path:
+                q.append(path+[next])
+
 
 
 def find_multiplier(path, dictionary):
     # Calculate conversion factor given path and dict
     multiplier = 1.0
     for i in range(len(path)-1):
-        multiplier /= dictionary[path[i]][path[i+1]]
+            multiplier /= dictionary[path[i]][path[i+1]]
     return multiplier
 
 
 def convert(n, start, end, dictionary, source):
     # Convert from start to end using conversion dictionary from create_conversions()
+    # 3 possible ways
     if source == start:
-        # 1 step
         multiplier = 1/dictionary[source][end] * float(n)
     elif source == end:
-        # 1 step
         multiplier = dictionary[source][start]*float(n)
     else:
-        # 2 steps
         multiplier = (1/(dictionary[start][source]*dictionary[source][end]))*float(n)
     return "{} {}".format(round(multiplier, 2), end)
 
@@ -64,20 +60,12 @@ def create_conversions(dictionary):
     # Takes the first key in dict as source
     source = list(dictionary.keys())[0]
     conversions = defaultdict(dict)
-    ends = set()
-    # Iterate over all units
-    # Adds all other units to a set
-    for key in dictionary.keys():
-        ends.add(key)
-        for x in dictionary[key].keys():
-            ends.add(x)
-    # Remove source from the set
-    ends.remove(source)
+    other_keys = list(dictionary.keys())[1:]
     # Create the dict and the reciprocals
-    for end in ends:
+    for end in other_keys:
         conversions[source][end] = 1/find_multiplier(find_path(source, end, dictionary), dictionary)
         conversions[end][source] = find_multiplier(find_path(source, end, dictionary), dictionary)
-    # Return created dict and source
+    # Return tuple of created dict and source
     return conversions, source
 
 
