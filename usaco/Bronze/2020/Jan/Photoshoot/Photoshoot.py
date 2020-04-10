34d877a53deb90bc78fefe31a1af683bce74e70c
MathiasOoi <60244997+MathiasOoi@users.noreply.github.com>
2020-04-09 21:22:58 -0800

    Update Photoshoot.py

--- usaco/Bronze/2020/Jan/Photoshoot/Photoshoot.py
+++ usaco/Bronze/2020/Jan/Photoshoot/Photoshoot.py
@@ -12,7 +12,12 @@ def photoshoot(i):
     return lst
 
 with open("photo.out", "w") as fout:
+    # Iterate over every number (i) from 1 to n+1
+    # Check if photoshoot(i) satisfies
+    # 1. Unique (No overlapping elements)
+    # 2. All elements in range of 1 to n+1
     for i in range(1, n+1):
         if len(set(photoshoot(i))) == n and set(photoshoot(i)) == set(range(1, n+1)):
+            # First list found that satisfies constraints will be lexicographically minimum
             fout.write(" ".join([str(s) for s in photoshoot(i)]))
             break
