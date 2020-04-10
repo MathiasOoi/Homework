08614431427169cbe29cfc5e420ec3f22fc08bd7
MathiasOoi <60244997+MathiasOoi@users.noreply.github.com>
2020-04-09 21:19:22 -0800

    Update Photoshoot.py

--- usaco/Bronze/2020/Jan/Photoshoot/Photoshoot.py
+++ usaco/Bronze/2020/Jan/Photoshoot/Photoshoot.py
@@ -1,23 +1,18 @@
-from itertools import permutations
 with open("photo.in") as fin:
     # Parse data
     n = int(fin.readline())
     cows = [int(i) for i in fin.readline().split()]
 
 
-def correct(iterable):
-    # For every i value in range of 0 to n - 1
-    # Check if a[i] + a[i+1] = b[i]
-    for i in range(n-1):
-        if iterable[i] + iterable[i+1] == cows[i]:
-            continue
-        return False
-    return True
+def photoshoot(i):
+    # Return list that satisfies a[i] + a[i+1] = b[i]
+    lst = [i]
+    for i in cows:
+        lst.append(i-lst[-1])
+    return lst
 
 with open("photo.out", "w") as fout:
-    # Permutations function returns permutations of an iterable in a lexicographically minimum order
-    # So the first permutation which satisfies correct() is the lexicographically minimum permutation
-    for perm in permutations(range(1, n+1)):
-        if correct(perm):
-            fout.write(" ".join([str(i) for i in perm]))
+    for i in range(1, n+1):
+        if len(set(photoshoot(i))) == n and set(photoshoot(i)) == set(range(1, n+1)):
+            fout.write(" ".join([str(s) for s in photoshoot(i)]))
             break
