d553c57211058c46c8a432ba99f6d7ed7fb24fde
MathiasOoi <60244997+MathiasOoi@users.noreply.github.com>
2020-04-09 17:27:13 -0800

    Update SumIsZero

--- SumIsZero.py
+++ SumIsZero.py
@@ -1,9 +1,12 @@
 def SumIsZero(lst):
     x = [[]]
+    # Create a list of lists of all possible sub-sets
     for i in lst:
         x.extend([k + [i] for k in x])
+    # Remove the empty list
     x = x[1:]
     print(x)
+    # Check the sums of all lists
     for i in x:
         if sum(i) == 0:
             return True
