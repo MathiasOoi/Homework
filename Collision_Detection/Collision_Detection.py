2334b53251aaf81ed2644744ee98d10221611086
MathiasOoi <60244997+MathiasOoi@users.noreply.github.com>
2020-05-05 16:34:53 -0800

    Update Collision_Detection (brute force)

--- Collision_Detection/Collision_Detection.py
+++ Collision_Detection/Collision_Detection.py
@@ -4,7 +4,7 @@ with open("simple.txt") as fin:
     rectangles = [(int(x), int(y), int(w), int(h)) for (x, y, w, h) in [i.split() for i in fin.readlines()]]
 
 def getPoints(rectangle):
-    # Takes in a list of [x, y, w, h]
+    # Takes in a tuple of (x, y, w, h)
     # Returns the 2 corners of a rectangle
     x, y, w, h = rectangle
     return [(x, y), (x+w, y+h)]
@@ -13,15 +13,14 @@ def intersect(rect1, rect2):
     # Check if a point is in the other rectangle
     (x1, y1), (x2, y2) = rect1
     (x3, y3), (x4, y4) = rect2
-    return (x1 < x3 < x2 and y1 < y3 < y2) or (x1 < x4 < x2 and y1 < y4 < y2)
-
+    return (x1 < x3 < x2 and y1 < y3 < y2) or (x1 < x4 < x2 and y1 < y4 < y2) or (x3 < x1 < x4 and y3 < y1 < y4) or (x3 < x2 < x4 and y3 < y2 < y4)
 
 def bruteForce():
     # Solves in O(N^2)
     result = set()
     for i, a in enumerate(rectangles):
         for k, b in enumerate(rectangles):
-            if a != b and intersect(getPoints(a), getPoints(b)):
+            if i != k and intersect(getPoints(a), getPoints(b)) and (k, i) not in result:
                 result.add((i, k))
     return result
 
