e088f0da54c0857c160a06bfa1da615f258a087c
MathiasOoi <60244997+MathiasOoi@users.noreply.github.com>
2020-09-26 13:31:37 -0700

    Update Wikipedia

--- Wikipedia/wikiparse.py
+++ Wikipedia/wikiparse.py
@@ -1,15 +1,14 @@
 import xml.etree.ElementTree as etree
 from collections import defaultdict
+import wikitextparser as wtp
 import sqlite3
 import string
 import time
 import os
 
-
 PATH_WIKI_XML = 'D:\\Wikipedia\\'
 FILENAME_WIKI = 'enwiki-20200901-pages-articles-multistream.xml'
 
-connection = sqlite3.connect('wiki.db')
 
 
 def hms_string(sec_elapsed):
@@ -20,11 +19,42 @@ def hms_string(sec_elapsed):
 
 
 def strip_tag_name(t):
-    for i in range(len(t)-1, 0, -1):
+    for i in range(len(t) - 1, 0, -1):
         if t[i] == "}":
-            return t[i+1: len(t)]
+            return t[i + 1: len(t)]
 
-def parsePage(title, text):
+
+def exclude(elem):
+    # Skip redirects
+    if elem.find('redirect') is not None:
+        return True
+    # Skip non-article pages.
+    # Note that you may need error handling
+    if elem.find('ns') is None or elem.find('ns').text != '0':
+        return True
+
+    # We don't have any reason to exclude it, so return
+    # False (keep it).
+    return False
+
+
+class WikiDB:
+    def __init__(self, filename):
+        self.conn = sqlite3.connect(filename)
+        self.conn.execute("""
+        CREATE TABLE IF NOT EXISTS articles (pageid integer, title text);
+        """)
+        self.conn.commit()
+    def insert(self, pageid, title):
+        self.conn.execute("""
+        INSERT INTO articles VALUES (?, ?);
+        """, (pageid, title))
+        self.conn.commit()
+
+
+
+
+def parsePage(elem, db):
     """
     Iterates over the page ONCE does a few things
     1) Determines the gender by counting pronouns
@@ -35,6 +65,14 @@ def parsePage(title, text):
     :param text: String wiki page
     :return: Tuple of (gender, info box, words)
     """
+    if not exclude(elem):
+        return
+    pageid = elem.find("id").text
+    title = elem.find('title').text
+    article = elem.find('revision/text').text
+    db.insert(pageid, title)
+    return
+
     def parseInfobox(title, infobox):
         """
         Helper function to help parse infobox
@@ -47,32 +85,30 @@ def parsePage(title, text):
             lineCounter += 1
             if line[0] == "|":  # Checks if that line has a new key
                 s = line.strip("| ")
-                for i in range(len(s)):
-                    if s[i] == "=":
-                        break
+                i = s.index("=")
                 key = s[:i].strip(" ")
                 try:
-                    if s[i+2] != "{":
+                    if s[i + 2] != "{":
                         for k in range(i, len(s)):
                             if s[k] == "<":  # removes an editor note "<!--- [note]"
                                 break
                         # Appends value to a key if that key only has ONE value
                         # I.e. "| children            = 8"
-                        d[key].append(s[i+2: k+1])
+                        d[key].append(s[i + 2: k + 1])
                 except IndexError:
                     continue
                 else:
-                    if s[i+4: i+9] == "hlist":
+                    if s[i + 4: i + 9] == "hlist":
                         # Parses out an hlist which is an horizontal list seperated by "|"
                         # i.e. "{{hlist|elem1|elem2|elem3}}"
-                        for k in range(i+9, len(s)):
+                        for k in range(i + 9, len(s)):
                             if s[k] == "<":
                                 break
                         # Split the hlist
-                        l = s[i+10: k].split("|")
+                        l = s[i + 10: k].split("|")
                         for item in l:  # Append every item in the hlist to the dict
                             d[key].append(item.strip("}").strip("{").strip("[[").strip("]]"))
-                    elif s[i+4: i+13] == "flatlist" or s[i+4: i+13] == "plainlist":
+                    elif s[i + 4: i + 13] == "flatlist" or s[i + 4: i + 13] == "plainlist":
                         # Parses out a flatlist and plainlist
                         # https://en.wikipedia.org/wiki/Template:Plainlist
                         # https://en.wikipedia.org/wiki/Template:Flatlist
@@ -81,7 +117,9 @@ def parsePage(title, text):
                                 break
                         lst = infobox[lineCounter: currlst]
                         for item in lst:
-                            d[key].append(item.strip("*").strip("{").strip("}").replace("[", "").replace("]", "").replace("end=div", ""))
+                            d[key].append(
+                                item.strip("*").strip("{").strip("}").replace("[", "").replace("]", "").replace(
+                                    "end=div", ""))
         return d
 
     def findInfobox(text):
@@ -105,7 +143,7 @@ def parsePage(title, text):
                 if not openCurly:
                     stop = i
                     break
-        return text[start: stop+1]
+        return text[start: stop + 1]
 
     def findGenderAndWords(text):
         """
@@ -117,7 +155,7 @@ def parsePage(title, text):
         :return: String
         """
         malePronoun = ["him", "his", "he", "he's"]
-        femalePronoun = ["her", "she","she's", "hers"]
+        femalePronoun = ["her", "she", "she's", "hers"]
         words = len(text)
         maleCount, femaleCount = 0, 0
         for word in text:
@@ -133,68 +171,30 @@ def parsePage(title, text):
         else:
             return "other", words
 
-
-
-    d = parseInfobox(title, findInfobox(text))
-    gender, words = findGenderAndWords(text.split())
-    print(d)
-    print(gender)
-    print(words)
-
-
-
-
-
-
+    #d = parseInfobox(title, findInfobox(text))
+    #gender, words = findGenderAndWords(text.split())
+    #print(d)
+    #print(gender)
+    #print(words)
 
 
 pathWikiXML = os.path.join(PATH_WIKI_XML, FILENAME_WIKI)
 
-totalCount = 0
-articleCount = 0
-redirectCount = 0
-templateCount = 0
-title = None
 start_time = time.time()
 
-with open("sample.txt", encoding="utf-8") as fin:
-    s = fin.read()
-    parsePage("aa", s)
-
-if __name__ == "__main__":
-    pass
-    for event, elem in etree.iterparse(pathWikiXML, events=('start', 'end')):
-        tname = strip_tag_name(elem.tag)
-        if event == 'start':
-            if tname == 'page':
-                title = ''
-                id = -1
-                redirect = ''
-                inrevision = False
-                ns = 0
-            elif tname == 'revision':
-                # Do not pick up on revision id's
-                inrevision = True
-        else:
-            if tname == 'title':
-                title = elem.text
-                print(title)
-            if tname == "text":
-                parsePage(title, elem.text)
-            elif tname == 'page':
-                totalCount += 1
-            if totalCount > 0:
-                break
-
-            if totalCount > 1 and (totalCount % 100000) == 0:
-                print("{:,}".format(totalCount))
-
+def main(file):
+    db = WikiDB("wiki.db")
+    for event, elem in etree.iterparse(file, events=('end',)):
+        elem.tag = strip_tag_name(elem.tag)
+        if elem.tag == "page":
+            parsePage(elem, db)
             elem.clear()
+            break
 
     elapsed_time = time.time() - start_time
 
-    print("Total pages: {:,}".format(totalCount))
-    print("Template pages: {:,}".format(templateCount))
-    print("Article pages: {:,}".format(articleCount))
-    print("Redirect pages: {:,}".format(redirectCount))
     print("Elapsed time: {}".format(hms_string(elapsed_time)))
+
+
+if __name__ == "__main__":
+    main(pathWikiXML)
