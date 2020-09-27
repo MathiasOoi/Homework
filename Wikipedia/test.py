9cf00d456fe5dc55148543edaff1419ba5792c23
mathiasooi <mathias7142@gmail.com>
2020-09-26 21:51:06 -0700

    Update Wikipedia

--- Wikipedia/test.py
+++ Wikipedia/test.py
@@ -1,5 +1,16 @@
-import xml.etree.ElementTree as etree
-import codecs
-import csv
-import time
-import os
+import wikitextparser as wtp
+
+
+with open("sample.txt") as fin:
+    parsed = wtp.parse(fin.read())
+    p = wtp.Table("""
+| honorific_suffix    = {{post-nominals|CBE}}
+| alt                 = Gilmour playing onstage
+| caption             = Gilmour performing in 2015
+| birth_name          = David Jon Gilmour
+    """)
+    for i in parsed.templates:
+        if "{{Infobox" in str(i):
+            print(i)
+            break
+
