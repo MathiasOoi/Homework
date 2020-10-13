7184d0b24d99cb22654cd731608c35f990cd120f
MathiasOoi <60244997+MathiasOoi@users.noreply.github.com>
2020-10-13 15:42:07 -0700

    Update

--- Wikipedia/newTable.py
+++ Wikipedia/newTable.py
@@ -1,8 +1,16 @@
 import zlib
+import time
 from wikidb import *
+from func_timeout import func_set_timeout
+from collections import defaultdict
+
 import wikitextparser as wtp
 
-db = WikiDB("wiki.db")
+def hms_string(sec_elapsed):
+    h = int(sec_elapsed / (60 * 60))
+    m = int((sec_elapsed % (60 * 60)) / 60)
+    s = sec_elapsed % 60
+    return "{}:{:>02}:{:>05.2f}".format(h, m, s)
 
 def getTemplate(page, template):
     """
@@ -19,10 +27,101 @@ def getTemplate(page, template):
 def getInfobox(page):
     article = wtp.parse(page)
     infobox = getTemplate(article, "Infobox")
-    print(infobox)
-    print(type(infobox))
+    return infobox
 
+def removeNotes(s):
+    """
+    text <--! note
+    Remove everything past the <!--
+    """
+    try:
+        return s[:s.index("<!--")]
+    except ValueError:
+        return s
 
-for (pageid, title, categories, article) in db.iterRandom(1):
-    print(title)
-    print(getInfobox(article))
+def getGender(page):
+    male, mc = ["he", "him", "his"], 0
+    female, fc = ["her", "she", "hers"], 0
+
+    for word in page.split():
+        if word in male:
+            mc += 1
+        elif word in female:
+            fc += 1
+
+    return "male" if mc > fc else "female"
+
+def parseCell(cell):
+    """
+    :param cell: String (Wikipedia infobox cell)
+    :return: Tuple (String, List)
+    """
+    cell = cell.strip("\n").strip("  ").strip("| ")
+    key, value = cell[:cell.find("=")].strip(" "), cell[cell.find("=") + 2:]
+    value = removeNotes(value)
+    if value.startswith("{{"):
+        value = value.strip("{{").strip("}}")
+        if value.startswith("hlist"):
+            value = value.strip("hlist").split("|")
+            return key, value[1:]
+        elif value.startswith("plainlist"):
+            value = value.split("\n")[1:-1]
+            return key, [v.strip("*{{").strip("}}") for v in value]
+        else:
+            return key, [value]
+
+    else:
+        return key, [value]
+
+@func_set_timeout(10)
+def parsePage(article):
+    try:
+        infobox = getInfobox(article)
+        if infobox is None:
+            return
+        infobox = infobox[infobox.find("\n") + 1: infobox.rfind("\n")]
+        if infobox.count("\n") == 0: return
+        infobox = wtp.Table(infobox)
+    except AttributeError:
+        return
+
+        # Iterate over every cell and parse it
+    d = defaultdict(list)
+    try:
+        for a in range(len(infobox.cells())):
+            for b in range(len(infobox.cells(row=a))):
+                parsed = parseCell(str(infobox.cells(row=a, column=b)))
+                d[parsed[0]] = parsed[1]
+    except IndexError:
+        return
+
+    gender = getGender(article)
+    args = repr(list(d.keys()))
+    chars = len(article)
+
+    return chars, gender, args
+def main():
+    newdb = WikiDBWithGenderAndInfobox("wikiWithoutArticle.db")
+    db = WikiDB("wiki.db")
+    pageCount = 0
+    start_time = time.time()
+    for (pageid, title, categories, article) in db:
+        print(title)
+        pageCount += 1
+        if not pageCount % 100000:
+            elapsed_time = time.time() - start_time
+            print("{} articles parsed".format((pageCount / 100000) * 100000), end=" ")
+            print("Elapsed time: {}".format(hms_string(elapsed_time)))
+        try:
+            chars, gender, args = parsePage(article)
+        except:
+            print("error")
+            continue
+        newdb.insert(pageid, title, categories, chars, gender, args)
+    newdb.commit()
+    elapsed_time = time.time() - start_time
+
+    print("Elapsed time: {}".format(hms_string(elapsed_time)))
+
+if __name__ == '__main__':
+    main()
